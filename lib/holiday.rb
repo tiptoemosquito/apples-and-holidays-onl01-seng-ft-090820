a266e68180881d131b90d96a421bab7b873b904b
Christina Howard <clhoward33@icloud.com>
2020-09-14 20:39:10 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -23,7 +23,8 @@ def second_supply_for_fourth_of_july(holiday_hash)
 end
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
-  holiday_supplies[:winter][:christmas][:new_years]
+  holiday_supplies[:winter][:christmas]="Balloons"
+  holiday_supplies[:winter][:new_years]="Balloons"
 
   # holiday_hash is identical to the one above
   # add the second argument, which is a supply, to BOTH the
